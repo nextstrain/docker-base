8c081135a85e3f1b18267d78afdde358803312b5
Thomas Sibley <tsibley@fredhutch.org>
2022-09-12 12:27:06 -0700

    Avoid littering the shell with the variables used for prompt formatting

--- prompt.sh
+++ prompt.sh
@@ -1,4 +1,5 @@
-RESET="\[\e[0m\]"
-BOLD="\[\e[1m\]"
-MAGENTA="\[\e[35m\]"
-PS1="${BOLD}nextstrain:${MAGENTA}\w${RESET} ${BOLD}\$ ${RESET}"
+reset="\[\e[0m\]"
+bold="\[\e[1m\]"
+magenta="\[\e[35m\]"
+PS1="${bold}nextstrain:${magenta}\w${reset} ${bold}\$ ${reset}"
+unset reset bold magenta
