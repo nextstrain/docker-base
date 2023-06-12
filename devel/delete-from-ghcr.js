7f025dcf2ac7c660fddf60b49fe2ada63ed159f9
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-06-12 14:56:59 -0700

    Delete dangling manifests
    Previously, only the manifest list (represented by the tag) would get
    deleted, leaving the individual manifests dangling without reference.
    
    This wasn't noticed often because if the manifest list was the last
    tagged version of the package, the entire package would get deleted
    (including individual manifests).

--- devel/delete-from-ghcr.js
+++ devel/delete-from-ghcr.js
@@ -1,4 +1,5 @@
-// Delete Nextstrain images with a specific tag from the GitHub Container registry.
+// Delete Nextstrain images associated with a tagged manifest list from the
+// GitHub Container registry.
 //
 // This file is intended for use by the GitHub action "actions/github-script".
 // To use locally, set `octokit`¹ as an Octokit instance with a personal access
@@ -6,7 +7,7 @@
 // ¹ https://github.com/octokit/core.js#authentication
 // ² https://github.com/settings/tokens/new?scopes=delete:packages
 
-module.exports = async ({octokit, tag}) => {
+module.exports = async ({fetch, octokit, tag, token}) => {
   org = 'nextstrain';
   packages = ['base', 'base-builder'];
 
@@ -14,6 +15,8 @@ module.exports = async ({octokit, tag}) => {
   let errorEncountered = false;
 
   for (const packageName of packages) {
+    // First, get a list of package "version" objects containing information
+    // used for deletion.
     let packageVersions;
     try {
       packageVersions = (await octokit.request('GET /orgs/{org}/packages/{package_type}/{package_name}/versions', {
@@ -27,6 +30,45 @@ module.exports = async ({octokit, tag}) => {
       errorEncountered = true;
       continue;
     }
+
+    // The manifest list + one manifest per platform are pushed from the build.
+    // Only the manifest list is tagged for direct removal via GitHub's REST API.
+
+    // The GitHub REST API does not provide a way to retrieve manifest digests
+    // from the manifest list, so use GHCR's (undocumented) Docker Registry API
+    // to do that.
+    // This works when a GitHub token with the right permissions is passed in
+    // base64 form as a Bearer token¹.
+    // ¹ https://github.com/orgs/community/discussions/26279#discussioncomment-3251172
+    const res = await fetch(`https://ghcr.io/v2/${org}/${packageName}/manifests/${tag}`,
+      {
+        headers: {
+          Accept: "application/vnd.docker.distribution.manifest.list.v2+json",
+          Authorization: `Bearer ${btoa(token)}`
+        }
+      });
+    const resData = await res.json();
+    const manifestDigests = resData.manifests.map(manifest => manifest.digest);
+
+    const versions = packageVersions.filter(version => manifestDigests.includes(version.name));
+    for (const version of versions) {
+      console.log(`Deleting the package version for ${org}/${packageName}:${version.name} ...`);
+      try {
+        await octokit.request('DELETE /orgs/{org}/packages/{package_type}/{package_name}/versions/{package_version_id}', {
+          org: org,
+          package_type: 'container',
+          package_name: packageName,
+          package_version_id: version.id,
+        });
+        console.log("Done.");
+      } catch (deleteVersionError) {
+        console.log(deleteVersionError);
+        errorEncountered = true;
+        continue;
+      }
+    }
+
+    // Delete the manifest list after deleting individual manifests.
 
     const versionsWithTag = packageVersions.filter(version => version.metadata.container.tags.includes(tag));
 
